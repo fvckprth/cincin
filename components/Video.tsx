3d55950d0a43cc9d68f62ca784226703be7a7933
fvckprth <parth@eastpark.xyz>
2023-10-09 11:20:40 -0400

    removed unmute

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,42 +1,50 @@
 "use client"
 
+// Video component
 import React, { useState, useRef } from 'react';
 import MuxPlayer from '@mux/mux-player-react';
 
-export default function BackgroundVideo() {
-    const [isMuted, setIsMuted] = useState(true);
-    const playerRef = useRef(null);
-
-    const handleMuteUnmute: React.MouseEventHandler<HTMLDivElement> = () => {
-        const current = playerRef.current;
-        if (current) {
-            (current as any).muted = !(current as any).muted;
-            setIsMuted(false);
-        }
-    };
-
-    return (
-        <div onClick={handleMuteUnmute}>
-            <MuxPlayer
-                ref={playerRef}
-                playbackId="aHuPzAYEtHo3QyPa01G9YcIttmSRUUvF6007rh00Op5oZM"
-                metadata={{
-                    video_title: "Italy MotoGP VHS Footage",
-                }}
-                autoPlay="any"
-                loop
-                streamType="on-demand"
-                playsInline
-                style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    width: "100%",
-                    height: "100%",
-                    zIndex: -1
-                }}
-                muted={isMuted}
-            />
-        </div>
-    );
+interface VideoProps {
+  playerRef: React.RefObject<any>;
 }
+
+const Video: React.FC<VideoProps> = ({ playerRef }) => {
+  const [isMuted, setIsMuted] = useState(true);
+
+  const handleMuteUnmute: React.MouseEventHandler<HTMLDivElement> = () => {
+    const current = playerRef.current;
+    if (current) {
+        (current as any).muted = !(current as any).muted;
+        setIsMuted(false);
+    }
+  };
+
+  return (
+    <div>
+    <div className='h-full w-full' onClick={handleMuteUnmute}>
+    </div>
+        <MuxPlayer
+        ref={playerRef}
+        playbackId="aHuPzAYEtHo3QyPa01G9YcIttmSRUUvF6007rh00Op5oZM"
+        metadata={{
+            video_title: "Italy MotoGP VHS Footage",
+        }}
+        autoPlay="any"
+        loop
+        streamType="on-demand"
+        playsInline
+        style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            zIndex: -1
+        }}
+        muted={isMuted}
+        />
+    </div>
+  );
+}
+
+export default Video;
