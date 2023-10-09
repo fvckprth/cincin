57f1b55a04a7839fd68273e45eecf46e09025964
fvckprth <parth@eastpark.xyz>
2023-10-09 11:52:31 -0400

    add next/link

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export default function CTA() {
     return (
-        <div className="flex items-center justify-center w-[85%] pb-12">
-            <Image
-                src="/cincin-cta.png"
-                alt="CinCin CTA"
-                width={1113}
-                height={470}
-            />            
+        <div className="flex items-center justify-center w-[85%] pb-12 z-[1000]">
+            <Link href="http://www.google.com">
+                <Image
+                    src="/cincin-cta.png"
+                    alt="CinCin CTA"
+                    width={1113}
+                    height={470}
+                />
+            </Link>            
         </div>
     )
 }
