1fc2b1d75e4dac51eecaffa6cbf564f43907030e
fvckprth <parth@eastpark.xyz>
2023-10-09 16:59:01 -0400

    updated link

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function CTA() {
     return (
         <div className="flex items-center justify-center w-[85%] pb-12 z-[1000]">
-            <Link href="http://www.google.com">
+            <Link href="https://invite.cincin.nyc/october-12">
                 <Image
                     src="/cincin-cta.png"
                     alt="CinCin CTA"
