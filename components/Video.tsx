902da75d3e2d434fc363d159f7b6f5154a2386a6
fvckprth <parth@eastpark.xyz>
2023-10-09 11:27:22 -0400

    Unmute

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,50 +1,54 @@
 "use client"
 
-// Video component
 import React, { useState, useRef } from 'react';
 import MuxPlayer from '@mux/mux-player-react';
 
-interface VideoProps {
-  playerRef: React.RefObject<any>;
+export default function BackgroundVideo() {
+    const [isMuted, setIsMuted] = useState(true);
+    const playerRef = useRef(null);
+
+    const handleMuteUnmute: React.MouseEventHandler<HTMLDivElement> = () => {
+        const current = playerRef.current;
+        if (current) {
+            (current as any).volume = isMuted ? 1 : 0;
+            setIsMuted(!isMuted);
+        }
+    };
+
+    return (
+        <div>
+            <MuxPlayer
+                ref={playerRef}
+                playbackId="aHuPzAYEtHo3QyPa01G9YcIttmSRUUvF6007rh00Op5oZM"
+                metadata={{
+                    video_title: "Italy MotoGP VHS Footage",
+                }}
+                autoPlay="any"
+                loop
+                streamType="on-demand"
+                playsInline
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    zIndex: -1
+                }}
+                volume={isMuted ? 0 : 1}
+            />
+            <div 
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: "100%",
+                    cursor: "pointer",
+                    backgroundColor: 'transparent'
+                }}
+                onClick={handleMuteUnmute}
+            />
+        </div>
+    );
 }
-
-const Video: React.FC<VideoProps> = ({ playerRef }) => {
-  const [isMuted, setIsMuted] = useState(true);
-
-  const handleMuteUnmute: React.MouseEventHandler<HTMLDivElement> = () => {
-    const current = playerRef.current;
-    if (current) {
-        (current as any).muted = !(current as any).muted;
-        setIsMuted(false);
-    }
-  };
-
-  return (
-    <div>
-    <div className='h-full w-full' onClick={handleMuteUnmute}>
-    </div>
-        <MuxPlayer
-        ref={playerRef}
-        playbackId="aHuPzAYEtHo3QyPa01G9YcIttmSRUUvF6007rh00Op5oZM"
-        metadata={{
-            video_title: "Italy MotoGP VHS Footage",
-        }}
-        autoPlay="any"
-        loop
-        streamType="on-demand"
-        playsInline
-        style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            zIndex: -1
-        }}
-        muted={isMuted}
-        />
-    </div>
-  );
-}
-
-export default Video;
