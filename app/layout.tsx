e1531b6d612c5c84cabd9a21fe2daa808d41f761
fvckprth <parth@eastpark.xyz>
2023-10-09 15:32:17 -0400

    huge commit 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
 
-
 export const metadata: Metadata = {
   title: '(Cin Cin) - NYC',
   description: 'Bringing Italian Aperitivo to New York City',
@@ -10,7 +9,22 @@ export const metadata: Metadata = {
     icon: '/favicon.ico',
     shortcut: '/favicon-16x16.png',
     apple: '/apple-touch-icon.png'
-  }
+  },
+  openGraph: {
+    title: '(Cin Cin) - NYC',
+    description: 'Bringing Italian Aperitivo to New York City',
+    url: 'https://ciccin.nyc',
+    images: [
+      {
+        url: '/cincin-og.png',
+        width: 800,
+        height: 450,
+        alt: 'Final Guest List',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
