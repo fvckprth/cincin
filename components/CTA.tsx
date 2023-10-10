610747a9b0a6fe47419d4315dc48d1c33730e644
fvckprth <parth@eastpark.xyz>
2023-10-10 01:13:18 -0400

    Replaced Qualify with RSVP

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -3,10 +3,10 @@ import Link from "next/link"
 
 export default function CTA() {
     return (
-        <div className="flex items-center justify-center w-[40%] md:w-[15%] pb-12 z-[1000]">
+        <div className="flex items-center justify-center w-[35%] md:w-[15%] pb-12 z-[1000]">
             <Link href="https://invite.cincin.nyc/october-12">
                 <Image
-                    src="/cincin-cta.png"
+                    src="/cincin-cta-rsvp.png"
                     alt="CinCin CTA"
                     width={1113}
                     height={470}
