065832809206cb763fc99704e59da24341252df6
fvckprth <parth@eastpark.xyz>
2023-10-10 20:09:34 -0400

    added external-link-cursor

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -5,7 +5,7 @@ export default function CTA() {
     return (
         <div className="flex items-center justify-center w-[35%] md:w-[15%] pb-12 z-[1000] custom-cursor">
             <Link href="https://invite.cincin.nyc/october-12">
-                <div className="custom-cursor">
+                <div className="external-link-cursor">
                     <Image
                         src="/cincin-cta-rsvp.png"
                         alt="CinCin CTA"
